b2779de96ea7e0089cac5c2138978c4f65439d7c
ychekan <iuriy.chekan@onix-systems.com>
2019-12-14 17:30:28 +0200

    added filter and search for search route

--- src/app/Services/SearchService.php
+++ src/app/Services/SearchService.php
@@ -13,12 +13,27 @@ class SearchService extends AppService
      * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
      * @throws NotFound
      */
-    public function getSearchData(string $keyword)
+    public function getSearchData(array $keyword)
     {
-        $data = Equipments::query()
-            ->where('custodian_name', 'ILIKE', '%' . strtolower($keyword) . '%')
-            ->orWhere('diagnostic_type', 'ILIKE', '%' . strtolower($keyword) . '%')
-            ->get();
+        $data = Equipments::query();
+
+        if (isset($keyword['q'])) {
+            $data->where(function ($q) use ($keyword) {
+                $q->where('custodian_name', 'ILIKE', '%' . strtolower($keyword['q']) . '%');
+                $q->orWhere('diagnostic_type', 'ILIKE', '%' . strtolower($keyword['q']) . '%');
+            });
+        }
+        if (isset($keyword['work-shedule'])) {
+            $data->where(['work_shedule' => $keyword['work-shedule']]);
+        }
+        if (isset($keyword['diagnostic-type'])) {
+            $data->where(['diagnostic_type' => $keyword['diagnostic-type']]);
+        }
+        if (isset($keyword['diagnostic-subgroup'])) {
+            $data->where(['diagnostic_subgroup' => $keyword['diagnostic-subgroup']]);
+        }
+
+        $data = $data->get();
 
         if (!count($data)) {
             throw new NotFound('Not found!');
