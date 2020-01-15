21fca16dc7529d52d2baa12a788510a328c83411
Dima Shchelko <dimashchelko@Dimas-MacBook-Pro.local>
2020-01-15 14:11:41 +0200

    add mobile view

--- src/resources/js/components/elements/Card/CardView.js
+++ src/resources/js/components/elements/Card/CardView.js
@@ -10,7 +10,7 @@ const CardView = ({
     <Accordion fluid>
       <Accordion.Title>
         <div className="container-fluid accordion-title__row accordion-title__row--margin-bottom no-padding">
-          <div className="col-11 card__custodian-name">
+          <div className="col-sm-11 card__custodian-name">
             {!expand ? equipment.custodian_name :
               <div className="container-fluid">
                 <div className="row">
@@ -26,7 +26,7 @@ const CardView = ({
             }
           </div>
           <div
-            className="col-1 d-flex justify-content-end"
+            className="col-sm-1 d-flex justify-content-sm-end arrow_toggle"
           >
             <ExpandIcon
               expand={expand}
@@ -50,7 +50,7 @@ const CardView = ({
           : ''}
         {!expand ?
           <div className="container-fluid accordion-title__bottom_row no-padding">
-            <div className="col-6 no-padding">
+            <div className="col-sm-6 no-padding">
               <div className="container-fluid no-padding">
                 <div className="accordion-title__bottom_row--margin">
                   <img src={MarkerIcon} className="accordion-title__marker-icon" alt="pin"/>
@@ -66,7 +66,7 @@ const CardView = ({
                 </div>
               </div>
             </div>
-            <div className="col-6 text-right no-padding">
+            <div className="col-sm-6 text-right no-padding">
               <button className="card__button col-sm-9 accordion-title__bottom_row--margin">
                 <span>ПОДІЛИТИСЯ</span>
               </button>
@@ -81,129 +81,129 @@ const CardView = ({
         <hr/>
         <div className="container-fluid no-padding">
           <div className="row">
-            <div className="col-4 card__title">Назва закладу</div>
-            <div className="col-8 card__description">{equipment.custodian_name}</div>
+            <div className="col-sm-4 card__title">Назва закладу</div>
+            <div className="col-sm-8 card__description">{equipment.custodian_name}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">ЄРДПОУ закладу</div>
-            <div className="col-8 card__description">{equipment.custodian_identifier}</div>
+            <div className="col-sm-4 card__title">ЄРДПОУ закладу</div>
+            <div className="col-sm-8 card__description">{equipment.custodian_identifier}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Назва структурного підрозділу, де знаходиться обладнання</div>
-            <div className="col-8 card__description">{equipment.structure_name}</div>
+            <div className="col-sm-4 card__title">Назва структурного підрозділу, де знаходиться обладнання</div>
+            <div className="col-sm-8 card__description">{equipment.structure_name}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Номер поверху</div>
-            <div className="col-8 card__description">{equipment.floor_number}</div>
+            <div className="col-sm-4 card__title">Номер поверху</div>
+            <div className="col-sm-8 card__description">{equipment.floor_number}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Номер кабінету</div>
-            <div className="col-8 card__description">{equipment.room_number}</div>
-          </div>
-        </div>
-        <hr/>
-        <div className="container-fluid no-padding">
-          <div className="row mt-3">
-            <div className="col-4 card__title">Група медичних послуг</div>
-            <div className="col-8 card__description">{equipment.inspection_type}</div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-4 card__title">Підгрупа медичних послуг</div>
-            <div className="col-8 card__description">{equipment.diagnostic_subgroup}</div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-4 card__title">Вид медичної послуги</div>
-            <div className="col-8 card__description">{equipment.diagnostic_type}</div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-4 card__title">Можливості навантаженості</div>
-            <div className="col-8 card__description">{equipment.diagnostic_quantity}</div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-4 card__title">Графік роботи кабінету, де працює обладнання</div>
-            <div className="col-8 card__description">{equipment.work_shedule}</div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-4 card__title">Обмеження прийому</div>
-            <div className="col-8 card__description">{equipment.availability_restriction}</div>
+            <div className="col-sm-4 card__title">Номер кабінету</div>
+            <div className="col-sm-8 card__description">{equipment.room_number}</div>
           </div>
         </div>
         <hr/>
         <div className="container-fluid no-padding">
           <div className="row mt-3">
-            <div className="col-4 card__title">Назва країни</div>
-            <div className="col-8 card__description">{equipment.address_country_name}</div>
+            <div className="col-sm-4 card__title">Група медичних послуг</div>
+            <div className="col-sm-8 card__description">{equipment.inspection_type}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Назва області</div>
-            <div className="col-8 card__description">{equipment.address_region}</div>
+            <div className="col-sm-4 card__title">Підгрупа медичних послуг</div>
+            <div className="col-sm-8 card__description">{equipment.diagnostic_subgroup}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Назва населеного пункту</div>
-            <div className="col-8 card__description">{equipment.address_locality}</div>
+            <div className="col-sm-4 card__title">Вид медичної послуги</div>
+            <div className="col-sm-8 card__description">{equipment.diagnostic_type}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Вулиця та номер будинку</div>
-            <div className="col-8 card__description">{equipment.address_street_address}</div>
+            <div className="col-sm-4 card__title">Можливості навантаженості</div>
+            <div className="col-sm-8 card__description">{equipment.diagnostic_quantity}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Географічна широта</div>
-            <div className="col-8 card__description">{equipment.latitude}</div>
+            <div className="col-sm-4 card__title">Графік роботи кабінету, де працює обладнання</div>
+            <div className="col-sm-8 card__description">{equipment.work_shedule}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Географічна довгота</div>
-            <div className="col-8 card__description">{equipment.longitude}</div>
+            <div className="col-sm-4 card__title">Обмеження прийому</div>
+            <div className="col-sm-8 card__description">{equipment.availability_restriction}</div>
           </div>
         </div>
         <hr/>
         <div className="container-fluid no-padding">
           <div className="row mt-3">
-            <div className="col-4 card__title">Назва обладнання</div>
-            <div className="col-8 card__description">{equipment.equipment_title}</div>
+            <div className="col-sm-4 card__title">Назва країни</div>
+            <div className="col-sm-8 card__description">{equipment.address_country_name}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Модель обладнання</div>
-            <div className="col-8 card__description">{equipment.equipment_model}</div>
+            <div className="col-sm-4 card__title">Назва області</div>
+            <div className="col-sm-8 card__description">{equipment.address_region}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Серійний номер</div>
-            <div className="col-8 card__description">{equipment.sn_equipment}</div>
+            <div className="col-sm-4 card__title">Назва населеного пункту</div>
+            <div className="col-sm-8 card__description">{equipment.address_locality}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Країна-виробник обладнання</div>
-            <div className="col-8 card__description">{equipment.producer_country}</div>
+            <div className="col-sm-4 card__title">Вулиця та номер будинку</div>
+            <div className="col-sm-8 card__description">{equipment.address_street_address}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Постачальник</div>
-            <div className="col-8 card__description">{equipment.producer_name}</div>
+            <div className="col-sm-4 card__title">Географічна широта</div>
+            <div className="col-sm-8 card__description">{equipment.latitude}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">ЄДРПОУ постачальника</div>
-            <div className="col-8 card__description">{equipment.producer_identifier}</div>
+            <div className="col-sm-4 card__title">Географічна довгота</div>
+            <div className="col-sm-8 card__description">{equipment.longitude}</div>
+          </div>
+        </div>
+        <hr/>
+        <div className="container-fluid no-padding">
+          <div className="row mt-3">
+            <div className="col-sm-4 card__title">Назва обладнання</div>
+            <div className="col-sm-8 card__description">{equipment.equipment_title}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Рік випуску обладнання</div>
-            <div className="col-8 card__description">{equipment.equipment_year}</div>
+            <div className="col-sm-4 card__title">Модель обладнання</div>
+            <div className="col-sm-8 card__description">{equipment.equipment_model}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Встановлений термін експлуатації виробником</div>
-            <div className="col-8 card__description">{equipment.equipment_life}</div>
+            <div className="col-sm-4 card__title">Серійний номер</div>
+            <div className="col-sm-8 card__description">{equipment.sn_equipment}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Рік введення в експлуатацію</div>
-            <div className="col-8 card__description">{equipment.registration_date}</div>
+            <div className="col-sm-4 card__title">Країна-виробник обладнання</div>
+            <div className="col-sm-8 card__description">{equipment.producer_country}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Експлуатаційний стан обладнання</div>
-            <div className="col-8 card__description">{equipment.equip_condition}</div>
+            <div className="col-sm-4 card__title">Постачальник</div>
+            <div className="col-sm-8 card__description">{equipment.producer_name}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Дата останнього ремонту</div>
-            <div className="col-8 card__description">{equipment.repair_date}</div>
+            <div className="col-sm-4 card__title">ЄДРПОУ постачальника</div>
+            <div className="col-sm-8 card__description">{equipment.producer_identifier}</div>
           </div>
           <div className="row mt-3">
-            <div className="col-4 card__title">Вид ремонтних робіт</div>
-            <div className="col-8 card__description">{equipment.repair_type}</div>
+            <div className="col-sm-4 card__title">Рік випуску обладнання</div>
+            <div className="col-sm-8 card__description">{equipment.equipment_year}</div>
+          </div>
+          <div className="row mt-3">
+            <div className="col-sm-4 card__title">Встановлений термін експлуатації виробником</div>
+            <div className="col-sm-8 card__description">{equipment.equipment_life}</div>
+          </div>
+          <div className="row mt-3">
+            <div className="col-sm-4 card__title">Рік введення в експлуатацію</div>
+            <div className="col-sm-8 card__description">{equipment.registration_date}</div>
+          </div>
+          <div className="row mt-3">
+            <div className="col-sm-4 card__title">Експлуатаційний стан обладнання</div>
+            <div className="col-sm-8 card__description">{equipment.equip_condition}</div>
+          </div>
+          <div className="row mt-3">
+            <div className="col-sm-4 card__title">Дата останнього ремонту</div>
+            <div className="col-sm-8 card__description">{equipment.repair_date}</div>
+          </div>
+          <div className="row mt-3">
+            <div className="col-sm-4 card__title">Вид ремонтних робіт</div>
+            <div className="col-sm-8 card__description">{equipment.repair_type}</div>
           </div>
         </div>
       </Accordion.Content>
--- src/resources/js/components/layouts/Body/BodyView.js
+++ src/resources/js/components/layouts/Body/BodyView.js
@@ -4,10 +4,10 @@ import Map from '../../elements/Map/Map';
 
 const BodyView = () => (
   <div className="body container-fluid">
-    <div className="col-6">
+    <div className="col-xl-6 order-xl-1 order-2">
       <List />
     </div>
-    <div className="col-6">
+    <div className="col-xl-6 order-xl-2 order-1">
       <Map />
     </div>
   </div>
